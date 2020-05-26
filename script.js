6d18142dd862865c11e135407a3967bf7e01bbc0
Kyle PC <kyle.dahl201@gmail.com>
2020-05-26 17:11:29 -0500

    adding questions and answers

--- script.js
+++ script.js
@@ -2,19 +2,30 @@
 
 var questions = [
 {
-    title: "",
-    choices: [],
-    answer: ""
+    title: "What technology is focused on styling a webpage?",
+    choices: ["HTML", "CSS", "JavaScript", "Sega Genesis"],
+    answer: "CSS"
 },
-
-
-
-
-
-
-
-
-
+{
+    title: "What value do booleans contain?",
+    choices: ["True/False", "Letters", "Numbers"],
+    answer: "True/False"
+},
+{
+    title: "What is used to loop through an array?",
+    choices: ["functions", "if/else", "For loops"],
+    answer: "For loops"
+},
+{
+    title: "Which is the newest version of HTML?",
+    choices: ["HTML2", "HTML3", "HTML5", "HTMLpizza"],
+    answer: "HTML5"
+},
+{
+    title: "What does HTML stand for?",
+    choices: ["Hyper type marked language", "Hyper text markup language", "Hyper typeface main loop", "Holy Toledo more lasagna!"],
+    answer: "Hyper text markup language"
+}
 
 
 
