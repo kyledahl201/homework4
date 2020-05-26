e0cc99f6fa4cb204c87c9afbc3eae40893999903
Kyle PC <kyle.dahl201@gmail.com>
2020-05-26 17:14:35 -0500

    writing variables for logic file

---
+++ logic.js
@@ -0,0 +1,9 @@
+//write variables to keep track of the quiz
+
+var currentQuestionIndex = 0:
+var time = questions.length * 15;
+var timerId;
+
+//variables to reference elements in the dom
+
+var questionsEl = document.getElementById("questions");
