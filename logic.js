f7733a4faf0b4e9a2ebd903880ee4ca0384c2d89
Kyle PC <kyle.dahl201@gmail.com>
2020-05-26 17:25:33 -0500

    beggining functions in logic file

--- logic.js
+++ logic.js
@@ -7,3 +7,44 @@ var timerId;
 //variables to reference elements in the dom
 
 var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+//function to start quiz
+function startQuiz() {
+//hide begin screen
+var startScreenEl = document.getElementById("start-screen");
+startScreenEl.setAttribute("class", "hide");
+
+//show questions
+
+questionsEl.removeAttribute("class");
+
+
+//start timer
+timerId = setInterval(clockTick, 1000);
+
+// show starting time
+timerEl.textContent = time;
+
+getQuestion();
+
+
+}
+
+function getQuestion() {
+
+//get the current question object 
+var curremtQuestion = questions[currentQuestionIndex];
+
+
+
+
+
+
+
+}
