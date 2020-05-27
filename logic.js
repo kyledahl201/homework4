f0cc0afef63fef3420917f3a2b3a7e1864c5bb89
Kyle PC <kyle.dahl201@gmail.com>
2020-05-26 19:54:46 -0500

    saving high score function

--- logic.js
+++ logic.js
@@ -140,7 +140,41 @@ function quizEnd() {
 
     }
 
-    
+    //function that saves score
+
+    function saveHighscore() {
+
+        // get value of input box
+
+        var initials = initialsEl.value.trim();
+
+        // make sure value wasn't empty 
+        if (initials !== "")  {
+//get saved scores or set to an array
+
+var highscores = 
+JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+//format the new object for the current player
+
+var newScore = {
+    score: time,
+    initials: initials
+}
+// save to localstorage
+highscores.push(newScore);
+window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+//redirect to next page
+window.location.href = "highscores.html";
+
+        }
+
+    }
+
+
+
+
 
 
 
